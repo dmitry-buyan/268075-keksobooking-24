ab8ad88b3a596cc2e17fa5a3c681ccf162a6356e
dmitry.buyan <buyandl@mail.ru>
2021-10-18 20:34:17 +0400

    activate/deactivate form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { generatePins } from './mock/pins.js';
 import { renderCard } from './card.js';
+import { deactivateForm, activateForm } from './form.js';
 
 const PINS_COUNT = 10;
 
 renderCard(generatePins(PINS_COUNT)[0]);
+deactivateForm();
+activateForm();
